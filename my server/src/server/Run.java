727ccf3c030097d305f7aed51328673391fba52a
dorhivert <dorhiv@gmail.com>
2015-09-10 12:59:31 +0300

    server

--- my server/src/server/Run.java
+++ my server/src/server/Run.java
@@ -1,9 +1,37 @@
 package server;
 
-public class Run {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-	public static void main(String[] args) {
+public class Run
+{
+	public static void main(String[] args) throws Exception
+	{
+		ServerSocket server = new ServerSocket(5400);
+		Socket someClient = server.accept();
+//		someClient.getInputStream()
 		
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(someClient.getInputStream()));
+		PrintWriter outToClient = new PrintWriter(someClient.getOutputStream());
+		
+		
+		String line;
+		while ( !(line = inFromClient.readLine()).equals("exit"))
+		{
+			StringBuilder sb = new StringBuilder(line);
+			outToClient.println(sb.reverse().toString());
+			outToClient.flush();
+		} 
+		outToClient.println("bye");
+		outToClient.flush();
+		
+		inFromClient.close();
+		outToClient.close();
+		someClient.close();
+		
+		server.close();
 	}
-
 }
