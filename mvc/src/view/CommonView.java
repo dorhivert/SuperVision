62dcd02a75eca5f319d2235086932b69b547a4b5
Tomhivert <tomhivert@gmail.com>
2015-09-19 18:25:59 +0300

    SNIPPET

--- mvc/src/view/CommonView.java
+++ mvc/src/view/CommonView.java
@@ -6,20 +6,14 @@ import mazeGenerators.Maze3d;
 public abstract class CommonView implements View{
 
 	Controller controller;
-	CLI cli;
 	
 	public CommonView(Controller controller,CLI cli)
 	{
-		this.cli = cli;
 		this.controller = controller;
 	}
 	
 	
-	public void setCLI(CLI cli)
-	{
-		this.cli = cli;
 
-	}
 	public void setController(Controller controller)
 	{
 		this.controller = controller;
@@ -28,21 +22,7 @@ public abstract class CommonView implements View{
 	{
 		return this.controller;
 	}
-	public CLI getCLI()
-	{
-		return cli;
-	}
-	
-	@Override
-	public void start() {
-		cli.start();
-	}
 
-	@Override
-	public void writeToConsole(String s) {
-		cli.out.println(s);
-		System.out.println(s);
-	}
 
 
 
