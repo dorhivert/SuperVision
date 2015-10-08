7edb375c5d5c86dafdfdeb575255433fed6fb995
dorhivert <dorhiv@gmail.com>
2015-10-08 19:16:13 +0300

    added testings

---
+++ mvp4/src/boot/Run.java
@@ -0,0 +1,28 @@
+package boot;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import model.MyModel;
+import presenter.Presenter;
+import view.CLI;
+import view.MyView;
+
+public class Run 
+{
+	public static void main(String[] args) 
+	{
+		MyModel model = new MyModel();
+		MyView view = new MyView();
+
+		Presenter presenter = new Presenter(model, view);
+		
+		view.setCli(new CLI(new BufferedReader(new InputStreamReader(System.in)), new PrintWriter(System.out), presenter.getCommandMap()));
+		
+		view.addObserver(presenter);
+		model.addObserver(presenter);
+		
+		view.start();
+	}
+}
