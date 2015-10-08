fb98c4fdb4d92f97671b91d7e40408b8043e90bd
dorhivert <dorhiv@gmail.com>
2015-10-08 20:53:52 +0300

    last for tonight?

---
+++ mvp4/src/presenter/Properties.java
@@ -0,0 +1,48 @@
+package presenter;
+
+import java.io.Serializable;
+
+public class Properties implements Serializable
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 555913593004844041L;
+	
+	private int numOfThreads;
+	private String generationAlgo;
+	private String solveAlgo;
+	
+	public Properties()
+	{
+		
+	}
+	
+	public Properties(int _num, String _gen, String _solve)
+	{
+		this.numOfThreads = _num;
+		this.generationAlgo = new String(_gen);
+		this.solveAlgo = new String(_solve);
+	}
+	
+	public int getNumOfThreads() {
+		return numOfThreads;
+	}
+	public void setNumOfThreads(int numOfThreads) {
+		this.numOfThreads = numOfThreads;
+	}
+	public String getGenerationAlgo() {
+		return generationAlgo;
+	}
+	public void setGenerationAlgo(String generationAlgo) {
+		this.generationAlgo = generationAlgo;
+	}
+	public String getSolveAlgo() {
+		return solveAlgo;
+	}
+	public void setSolveAlgo(String solveAlgo) {
+		this.solveAlgo = solveAlgo;
+	}
+
+}
