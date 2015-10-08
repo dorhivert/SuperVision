e92d31c96dff1d299ad6adc4dabb808db11b09a0
Tomhivert <tomhivert@gmail.com>
2015-10-08 20:58:56 +0300

    last for tonight

--- mvp/src/boot/Run.java
+++ mvp/src/boot/Run.java
@@ -1,30 +1,31 @@
 package boot;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
-import model.Model;
-import model.MyModel;
-import presenter.Presenter;
-import view.CLI;
-import view.MyView;
-import view.View;
+import presenter.Properties;
 
 public class Run {
 
 	public static void main(String[] args) {
-		Model model = new MyModel();
-		View view = new MyView();
-		
-		Presenter presenter = new Presenter(model,view);
-		
-		((MyView) view).setCli(new CLI(new BufferedReader(new InputStreamReader(System.in)), new PrintWriter(System.out), presenter.getCommandMap()));
-
-		((MyView) view).addObserver(presenter);
-		((MyModel) model).addObserver(presenter);
-		
-		view.start();
+//		Model model = new MyModel();
+//		View view = new MyView();
+//		
+//		Presenter presenter = new Presenter(model,view);
+//		
+//		((MyView) view).setCli(new CLI(new BufferedReader(new InputStreamReader(System.in)), new PrintWriter(System.out), presenter.getCommandMap()));
+//
+//		((MyView) view).addObserver(presenter);
+//		((MyModel) model).addObserver(presenter);
+//		
+//		view.start();
+		Properties prop = new Properties();
+		try {
+			prop.writeProperties("properties.xml");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(prop.getGenerationAlgo());
 	}
 
 }
