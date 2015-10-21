b56d64b0f52c8fdb55460d7ffcd72868c7d195e9
Tomhivert <tomhivert@gmail.com>
2015-10-21 17:07:20 +0300

    up/down colors

--- mvp11/src/view/MyMazeDisplayWidget.java
+++ mvp11/src/view/MyMazeDisplayWidget.java
@@ -8,14 +8,14 @@ import org.eclipse.swt.widgets.Composite;
 public class MyMazeDisplayWidget extends CommonMazeDisplayWidget {
 
 	private Color black,white;
-	
+
 	MyMazeDisplayWidget(Composite composite, int style) {
 		super(composite, style);
 		black = new Color(getDisplay(), 0, 0, 0);
 		white = new Color(getDisplay(), 255, 255 ,255);
-		
+
 		addPaintListener(new PaintListener() {
-			
+
 			@Override
 			public void paintControl(PaintEvent arg0) {
 
@@ -29,44 +29,71 @@ public class MyMazeDisplayWidget extends CommonMazeDisplayWidget {
 				else {
 					min = getSize().x;
 				}
-				int levelSelected = 5;
-//		        Color c1 = new Color(arg0.display, 50, 50, 200);
-//		        arg0.gc.setBackground(c1);
-//		        arg0.gc.fillRectangle(10, 15, 90, 60);
+				int levelSelected = 2;
+//				Color c1 = new Color(arg0.display, 50, 50, 200);
+//				arg0.gc.setBackground(c1);
+//				arg0.gc.fillRectangle(10, 15, 90, 60);
 //
-//		        Color c2 = new Color(arg0.display, 105, 90, 60);
-//		        arg0.gc.setBackground(c2);
-//		        arg0.gc.fillRectangle(130, 15, 90, 60);
+//				Color c2 = new Color(arg0.display, 105, 90, 60);
+//				arg0.gc.setBackground(c2);
+//				arg0.gc.fillRectangle(130, 15, 90, 60);
 //
-//		        Color c3 = new Color(arg0.display, 33, 200, 100);
-//		        arg0.gc.setBackground(c3);
-//		        arg0.gc.fillRectangle(250, 15, 90, 60);
-				
+//				Color c3 = new Color(arg0.display, 33, 200, 100);
+//				arg0.gc.setBackground(c3);
+//				arg0.gc.fillRectangle(250, 15, 90, 60);
+
 				int cellSize = min / getMyMaze().getMaze().length;
-				
-				for(int x = 0;x<getMyMaze().getMaxX();x++)
+				//drawing 2d array
+				for(int x = 0;x<getMyMaze().getMaxX()+1;x++)
 				{
-					for (int y = 0; y < getMyMaze().getMaxY(); y++)
+					for (int y = 0; y < getMyMaze().getMaxY()+1; y++)
 					{
 						
+						startFromX = x*cellSize;
+						startFromY = y*cellSize;
+						arg0.gc.drawRectangle(startFromX, startFromY, cellSize, cellSize);
+						
+							
+
+
+						if(getMyMaze().getCellValue(x, y, levelSelected)==0)
 						{
-
-
-							startFromX = x*cellSize;
-							startFromY = y*cellSize;
-							arg0.gc.drawRectangle(startFromX, startFromY, cellSize, cellSize);
-
-
-							if(getMyMaze().getCellValue(x, y, levelSelected)==0)
+							Color bg = new Color(arg0.display,200,200,200);
+							arg0.gc.setBackground(bg);
+							arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+							bg.dispose();
+							if(getMyMaze().getCellValue(x, y, levelSelected+1)==0)
 							{
-								Color bg = new Color(arg0.display,0,255,255);
-								arg0.gc.setBackground(bg);
-								arg0.gc.fillRectangle(startFromX+1, startFromY+1, cellSize-1, cellSize-1);
+								Color upAvailable = new Color(arg0.display,0,0,200);
+								arg0.gc.setBackground(upAvailable);
+								arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
 								bg.dispose();
-			
+							}
+							if(getMyMaze().getCellValue(x, y, levelSelected-1)==0)
+							{
+								Color downAvailable = new Color(arg0.display,0,200,0);
+								arg0.gc.setBackground(downAvailable);
+								arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+								bg.dispose();
+							}
+							if((getMyMaze().getCellValue(x, y, levelSelected-1)==0)&&(getMyMaze().getCellValue(x, y, levelSelected+1)==0))
+							{
+								Color upAndDownAvailable = new Color(arg0.display,0,200,200);
+								arg0.gc.setBackground(upAndDownAvailable);
+								arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+								bg.dispose();
 							}
 						}
-						
+						else
+						{
+							Color bg = new Color(arg0.display,0,0,0);
+							arg0.gc.setBackground(bg);
+							arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+							bg.dispose();
+
+						}
+
+
 					}
 				}
 				setBackground(white);
@@ -78,45 +105,45 @@ public class MyMazeDisplayWidget extends CommonMazeDisplayWidget {
 	@Override
 	public void moveUp() {
 		getMyMaze().moveUp();
-		
+
 	}
 
 	@Override
 	public void moveDown() {
 		getMyMaze().moveDown();
-		
+
 	}
 
 	@Override
 	public void moveLeft() {
 		getMyMaze().moveLeft();
-		
+
 	}
 
 	@Override
 	public void moveRight() {
 		getMyMaze().moveRight();
-		
+
 	}
 
 	@Override
 	public void moveIn() {
 		getMyMaze().moveIn();
-		
+
 	}
 
 	@Override
 	public void moveOut() {
 		getMyMaze().moveOut();
-		
+
 	}
 
 	@Override
 	public void winGame() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+
 
 }
