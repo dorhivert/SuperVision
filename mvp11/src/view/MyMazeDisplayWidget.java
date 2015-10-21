bb1553898ff31e9f90d960a1d0cc050053b59ada
Tomhivert <tomhivert@gmail.com>
2015-10-21 15:58:21 +0300

    updated mazedisplay

--- mvp11/src/view/MyMazeDisplayWidget.java
+++ mvp11/src/view/MyMazeDisplayWidget.java
@@ -1,12 +1,9 @@
 package view;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 
 public class MyMazeDisplayWidget extends CommonMazeDisplayWidget {
 
@@ -25,12 +22,25 @@ public class MyMazeDisplayWidget extends CommonMazeDisplayWidget {
 				int startFromX = 0;
 				int startFromY = 0;
 				setBackground(white);
+//				setBackground(new Color(getDisplay(), 0, 255, 255));
 				int min=0;
 				if(getSize().y<getSize().x)
 					min=getSize().y;
 				else {
 					min = getSize().x;
 				}
+				int levelSelected = 5;
+//		        Color c1 = new Color(arg0.display, 50, 50, 200);
+//		        arg0.gc.setBackground(c1);
+//		        arg0.gc.fillRectangle(10, 15, 90, 60);
+//
+//		        Color c2 = new Color(arg0.display, 105, 90, 60);
+//		        arg0.gc.setBackground(c2);
+//		        arg0.gc.fillRectangle(130, 15, 90, 60);
+//
+//		        Color c3 = new Color(arg0.display, 33, 200, 100);
+//		        arg0.gc.setBackground(c3);
+//		        arg0.gc.fillRectangle(250, 15, 90, 60);
 				
 				int cellSize = min / getMyMaze().getMaze().length;
 				
@@ -38,7 +48,7 @@ public class MyMazeDisplayWidget extends CommonMazeDisplayWidget {
 				{
 					for (int y = 0; y < getMyMaze().getMaxY(); y++)
 					{
-						for (int z = 0; z < getMyMaze().getMaxZ(); z++)
+						
 						{
 
 
@@ -46,15 +56,21 @@ public class MyMazeDisplayWidget extends CommonMazeDisplayWidget {
 							startFromY = y*cellSize;
 							arg0.gc.drawRectangle(startFromX, startFromY, cellSize, cellSize);
 
-//							if(getMyMaze().getCellValue(x, y, z)==1)
-//							{
-//								arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
-//							}
+
+							if(getMyMaze().getCellValue(x, y, levelSelected)==0)
+							{
+								Color bg = new Color(arg0.display,0,255,255);
+								arg0.gc.setBackground(bg);
+								arg0.gc.fillRectangle(startFromX+1, startFromY+1, cellSize-1, cellSize-1);
+								bg.dispose();
+			
+							}
 						}
 						
 					}
 				}
-				
+				setBackground(white);
+
 			}
 		});
 	}
