69cef59d14ed5301f72a24f81d475cb95e89ae8b
dorhivert <dorhiv@gmail.com>
2015-10-21 17:38:28 +0300

    new mvp

---
+++ DORmvp12/src/view/MyGUIView.java
@@ -0,0 +1,254 @@
+package view;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import mazeGenerators.Maze3d;
+import mazeGenerators.Maze3dGenerator;
+import mazeGenerators.MyMaze3dGenerator;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Text;
+
+import solution.Solution;
+
+public class MyGUIView extends BasicWindow implements Closeable
+{
+	private String propertiesFilePath;
+	private Text ascii;
+	private MessageBox msgs;
+	public CommonMazeDisplayWidget mazeDisplay;
+
+	public MyGUIView(String title, int width, int height)
+	{
+		super(title, width, height);
+	}
+
+	@Override
+	public void start()
+	{
+		run();
+	}
+
+	@Override
+	public void writeToConsole(String userInput){} 
+	@Override
+	public void displayDirectory(String[] path){}
+	@Override
+	public void displayCommandMenu(){}
+
+
+	@Override
+	public void displayCrossSection(int[][] crossSection)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayMazeSize(String name, double mazeSize)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayFileSize(String name, double fileSize) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayMaze(Maze3d maze)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displaySolution(Solution s)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void InitWidgets()
+	{
+		shell.setLayout(new GridLayout(2, false));
+		shell.addListener(SWT.Close, new Listener() 
+		{
+			@Override
+			public void handleEvent(Event arg0)
+			{
+				int style = SWT.APPLICATION_MODAL | SWT.YES | SWT.NO;
+				changeAndNotify("exit");
+				msgs = new MessageBox(shell, style);
+				msgs.setText("Information");
+				msgs.setMessage("Close the shell?");
+				arg0.doit = msgs.open() == SWT.YES;
+			}
+		});
+		Button open = new Button(shell, SWT.PUSH);
+		open.setLayoutData(new GridData(SWT.FILL, SWT.None, false, false, 1, 1));
+		open.setText("load XML properties file");
+		open.addSelectionListener(new SelectionListener()
+		{
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {}
+			@Override
+			public void widgetSelected(SelectionEvent arg0)
+			{
+				FileDialog fd=new FileDialog(shell,SWT.OPEN);
+				fd.setText("open");
+				fd.setFilterPath("");
+				String[] filterExt = { "*.xml", "*.XML", "*.*" };
+				fd.setFilterExtensions(filterExt);
+				propertiesFilePath = fd.open();
+				if (propertiesFilePath != null)
+				{
+					String line = new String("openNewXML");
+					line = line+" "+propertiesFilePath;
+					changeAndNotify(line);
+					msgs = new MessageBox(shell);
+					msgs.setText("NOTICE");
+					msgs.setMessage("You must restart the apllication in order to apply new properties!");
+					msgs.open();
+				}
+			}
+		});
+
+		Button generate = new Button(shell, SWT.PUSH);
+		generate.setLayoutData(new GridData(SWT.FILL, SWT.None, false, false, 1, 1));
+		generate.setText("Generate 3d Maze!");
+		generate.addSelectionListener(new SelectionListener()
+		{
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0){}
+			@Override
+			public void widgetSelected(SelectionEvent arg0)
+			{
+				MazeSettingsDialog fd=new MazeSettingsDialog(shell);
+				fd.setText("open");
+
+				String newValue = new String(fd.open());
+
+				String line = new String("generate 3d maze");
+				line = line+" "+newValue;
+				String [] splittedLine = line.split(" ");
+				int num = splittedLine.length;
+				boolean flag = true;
+				if (num == 5) 
+				{
+					if (!splittedLine[3].equals("unNamed Maze")) 
+					{
+						if (splittedLine[3] == null)
+						{
+							flag = false;
+						}
+						if (Integer.parseInt(splittedLine[4]) < 1) 
+						{
+							flag = false;
+						}
+						if (flag == true)
+						{
+							ascii.append("IM GENERATING!\n");
+							changeAndNotify(line);
+						}
+					}
+				}
+			}
+		});
+
+		Button mazeProps = new Button(shell, SWT.PUSH);
+		mazeProps.setLayoutData(new GridData(SWT.FILL, SWT.None, false, false, 1, 1));
+		mazeProps.setText("Application Properties");
+		mazeProps.addSelectionListener(new SelectionListener()
+		{
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0){}
+			@Override
+			public void widgetSelected(SelectionEvent arg0)
+			{
+				GamePropertiesDialog gpd = new GamePropertiesDialog(shell);
+				gpd.setText("open");
+
+				String newValue = new String(gpd.open());
+
+				String line = new String("changeProperties ");
+				line = line+newValue;
+				if (line.split(" ").length == 5)
+				{
+					changeAndNotify(line);
+					msgs = new MessageBox(shell);
+					msgs.setText("NOTICE");
+					msgs.setMessage("You must restart the apllication in order to apply new properties!");
+					msgs.open();
+				}
+			}
+		});
+
+//		ascii = new Text(shell, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+//		ascii.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 2));
+		
+		mazeDisplay = new MyMazeDisplayWidget(shell, SWT.BORDER);
+		mazeDisplay.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 2));
+		Maze3dGenerator mg = new MyMaze3dGenerator();
+		Maze3d myMaze = mg.generate(15, 15, 15);
+		mazeDisplay.setMyMaze(myMaze);
+		mazeDisplay.redraw();
+
+		Button egzit = new Button(shell, SWT.PUSH);
+		egzit.setLayoutData(new GridData(SWT.None, SWT.None, false, false, 1, 1));
+		egzit.setText("EXIT");
+		egzit.addSelectionListener(new SelectionListener()
+		{
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0){}
+			@Override
+			public void widgetSelected(SelectionEvent arg0) 
+			{
+				try 
+				{
+					close();
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	@Override
+	public void close() throws IOException
+	{
+		changeAndNotify("exit");
+		if (display!=null&&(!display.isDisposed()))
+		{
+			display.dispose();
+		}
+
+		if(shell!=null&&(!shell.isDisposed()))
+		{
+			shell.dispose();
+		}
+	}
+
+	private void changeAndNotify(String command)
+	{
+		String [] splittedLine = command.split(" ");
+		setChanged();
+		notifyObservers(splittedLine);
+	}
+}
---
+++ DORmvp12/src/view/MyMazeDisplayWidget.java
@@ -0,0 +1,149 @@
+package view;
+
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Composite;
+
+public class MyMazeDisplayWidget extends CommonMazeDisplayWidget 
+{
+	private GameCharacter myMegi;
+
+
+	MyMazeDisplayWidget(Composite composite, int style) 
+	{
+		super(composite, style);
+		myMegi = new GameCharacter();
+
+		addPaintListener(new PaintListener()
+		{
+
+			@Override
+			public void paintControl(PaintEvent arg0)
+			{
+
+				int startFromX = 0;
+				int startFromY = 0;
+				setBackground(white);
+				//				setBackground(new Color(getDisplay(), 0, 255, 255));
+				int min=0;
+
+				if(getSize().y < getSize().x)
+					min=getSize().y+2;
+				else 
+					min = getSize().x+2;
+
+				int levelSelected = 5;
+				//		        Color c1 = new Color(arg0.display, 50, 50, 200);
+				//		        arg0.gc.setBackground(c1);
+				//		        arg0.gc.fillRectangle(10, 15, 90, 60);
+				//
+				//		        Color c2 = new Color(arg0.display, 105, 90, 60);
+				//		        arg0.gc.setBackground(c2);
+				//		        arg0.gc.fillRectangle(130, 15, 90, 60);
+				//
+				//		        Color c3 = new Color(arg0.display, 33, 200, 100);
+				//		        arg0.gc.setBackground(c3);
+				//		        arg0.gc.fillRectangle(250, 15, 90, 60);
+
+				int cellSize = min / getMyMaze().getMaze().length;
+
+
+				arg0.gc.setBackground(black);
+				arg0.gc.fillRectangle(0, 0, cellSize-1, cellSize-1);
+
+				for(int x = 0;x<getMyMaze().getMaxX()+1;x++)
+				{
+					for (int y = 0; y < getMyMaze().getMaxY()+1; y++)
+					{
+						{
+							startFromX = x*cellSize;
+							startFromY = y*cellSize;
+							arg0.gc.drawRectangle(startFromX, startFromY, cellSize, cellSize);
+
+
+							if(getMyMaze().getCellValue(x, y, levelSelected)==0)
+							{
+								arg0.gc.setBackground(white);
+								arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+								if(getMyMaze().getCellValue(x, y, levelSelected+1)==0)
+								{
+			
+									arg0.gc.setBackground(superLightBlack);
+									arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+				
+								}
+								if(getMyMaze().getCellValue(x, y, levelSelected-1)==0)
+								{
+									
+									arg0.gc.setBackground(superLightBlack);
+									arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+									
+								}
+								if((getMyMaze().getCellValue(x, y, levelSelected-1)==0)&&(getMyMaze().getCellValue(x, y, levelSelected+1)==0))
+								{
+									
+									arg0.gc.setBackground(superLightBlack);
+									arg0.gc.fillRectangle(startFromX, startFromY, cellSize, cellSize);
+									
+								}
+							}
+							else
+							{
+								arg0.gc.setBackground(black);
+								arg0.gc.fillRectangle(startFromX+1, startFromY+1, cellSize-1, cellSize-1);
+							}
+						}
+					}
+				}
+				myMegi.print(arg0, 0, 0, cellSize, cellSize);
+				setBackground(white);
+			}
+		});
+	}
+
+	@Override
+	public void moveUp() {
+		getMyMaze().moveUp();
+
+	}
+
+	@Override
+	public void moveDown() {
+		getMyMaze().moveDown();
+
+	}
+
+	@Override
+	public void moveLeft() {
+		getMyMaze().moveLeft();
+
+	}
+
+	@Override
+	public void moveRight() {
+		getMyMaze().moveRight();
+
+	}
+
+	@Override
+	public void moveIn() {
+		getMyMaze().moveIn();
+
+	}
+
+	@Override
+	public void moveOut() {
+		getMyMaze().moveOut();
+
+	}
+
+	@Override
+	public void winGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+}
