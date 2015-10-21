e8ac7c93aea1a76ecf87188813666c0d0a735c40
Tomhivert <tomhivert@gmail.com>
2015-10-21 19:05:29 +0300

    gift

---
+++ tomMVP/src/view/GiftImage.java
@@ -0,0 +1,16 @@
+package view;
+
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.graphics.Image;
+
+public class GiftImage {
+	
+	public GiftImage(){}
+	
+	public void print(PaintEvent e, int pointX, int pointY, int width, int height) 
+	{
+		Image i = new Image(null, "./resources/gift.png");
+		e.gc.drawImage(i, 0, 0, 1600, 1568, pointX, pointY, width, height);
+	}
+
+}
