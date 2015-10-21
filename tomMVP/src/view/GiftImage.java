4ebc02b077c9c9120da7f95cd302f9eb5f5f8bbd
Tomhivert <tomhivert@gmail.com>
2015-10-21 19:07:57 +0300

    fixed giftImage

--- tomMVP/src/view/GiftImage.java
+++ tomMVP/src/view/GiftImage.java
@@ -3,10 +3,10 @@ package view;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.graphics.Image;
 
-public class GiftImage {
+public class GiftImage implements GameChar{
 	
 	public GiftImage(){}
-	
+	@Override
 	public void print(PaintEvent e, int pointX, int pointY, int width, int height) 
 	{
 		Image i = new Image(null, "./resources/gift.png");
